dce55006cf81a254f7fb150248fa24ad8d168ca7
Ian Cavalcante <iancavalcante10@gmail.com>
2022-09-21 04:16:41 -0300

    Adicionado pasta TRELLO

---
+++ Trello/src/Git4questoes.java
@@ -0,0 +1,56 @@
+
+public class Git4questoes {
+
+	/*
+	 * Ex 1 limite W.I.P Kanban O work in progress permite o usuario administrar o
+	 * processo de uma maneira que crie um fluxo eficaz, cujo previna a sobrecarga
+	 * do trabalho. A implementacao permite maior foco nas tarefas importantes,
+	 * predefinindo pré requisitos para o avanco das tarefas. Previne multitask. A
+	 * otimizacao dos processos é notória. Setar o máximo de tarefas a serem
+	 * realizadas, com o intuito de um trabalho mais focado, tornando mais fácil a
+	 * identificacao de área de melhora.
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * Ex2 Informe qual comando git gera um pacote de alteracoes feitas no
+	 * repositorio contendo arquivos alterados, autor e uma mensagem de resumo.
+	 * 
+	 * git reflog
+	 * git log
+	 *
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * Ex3 Informe qual comando git usamos para verificar a situacao atual do
+	 * repositorio e da staging area.
+	 * 
+	 * git status 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * Ex4 Informe qual comando git usamos para listar os branchs
+	 * disponiveis/existentes.
+	 * 
+	 * 
+	 * git branch
+	 * 
+	 * 
+	 * 
+	 *
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+}
