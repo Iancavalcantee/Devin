359b270d433372b32dae3686fac4067bc0423612
Ian Cavalcante <iancavalcante10@gmail.com>
2022-09-26 04:26:00 -0300

    versao 26.09

--- Trello/src/M01S02Ex05.java
+++ Trello/src/M01S02Ex05.java
@@ -1,25 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class M01S02Ex05 {
 
 	public static void main(String[] args) {
 
-		/*
-		 * Implemente uma função 'calcular' que receba como parâmetro um array
-		 * (numérico) e identifique o maior e menor valor do mesmo. Imprima no console
-		 * estes valores ou "Não é possível calcular" qdo não for possível.
-		 * 
-		 * Teste a função com as seguintes entradas:
-		 * 
-		 * - {56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47} - {1} - {1, -1} -
-		 * null - {-2,-2,-2,-2} - {20,10, 30}
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 */
-	}
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Digite a quantidade de elementos: ");
+		int x = sc.nextInt();
+
+		int valor[] = new int[x];
+		int maior = 0, menor = 999999999;
+
+		if (x == 1) {
+			System.out.println("Situacao inexistente ");
+
+		}
+
+		else {
+
+			for (int i = 0; i < valor.length; i++) {
+				System.out.println("Digite o " + (i + 1) + " valor:");
+				valor[i] = sc.nextInt();
+
+				if (valor[i] > maior) {
+					maior = valor[i];
+
+				}
+
+			}
+
+			for (int j = 0; j < valor.length; j++) {
+				if (valor[j] < menor) {
+					menor = valor[j];
+				}
+
+			}
+
+			System.out.println("Maior valor " + maior);
+			System.out.println("Menor valor " + menor);
+
+		}
+	}
 }
+
+/*
+ * 
+ * 
+ * 
+ * Implemente uma função 'calcular' que receba como parâmetro um array
+ * (numérico) e identifique o maior e menor valor do mesmo. Imprima no console
+ * estes valores ou "Não é possível calcular" qdo não for possível.
+ * 
+ * Teste a função com as seguintes entradas:
+ * 
+ * - {56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47} - {1} - {1, -1} -
+ * null - {-2,-2,-2,-2} - {20,10, 30}
+ * 
+ */
--- Trello/src/M01S02Ex06.java
+++ Trello/src/M01S02Ex06.java
@@ -1,9 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class M01S02Ex06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// List<String> lista = new ArrayList<>();
+
+		// List<Integer> lista = Arrays.asList(1,2,3,4);
+		int[] lista1 = { 1, 2, 3, 4 };
+
+		for (int i = 0; i < lista1.length; i++) {
+
+			//List<Integer> lista3x = Arrays.asList(lista1[i] * 3);
+		 int x = lista1[i]* 3 ;
+			List<Integer> lista3x = new ArrayList<>();
+			lista3x.add(x);
+			
+			System.out.println("lista3x: " + lista3x);
+
+		}
+
+		
+		
 	}
 
 }
